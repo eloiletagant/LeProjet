624ddb2d9ae29e5e4f376cbf00c9445d9e70386b
Alexis A <alexis.amiand@gmail.com>
2017-04-06 15:22:37 +0200

    THE GAME

--- LeProjet/src/java/game/Game.java
+++ LeProjet/src/java/game/Game.java
@@ -5,87 +5,72 @@
  */
 package game;
 
-/**
- *
- * @author 
- */
-public class Game implements API{
-private static final byte[][] matrix =null; // matrice des pions de 1-7 (largeur) 1-6, 1-7 (hauteur)
-private int winner; // 0: null, 1 or 2 the winner
-private int lastPlayer;
-private byte[2] lastCell = [0,0]; // stock la derniere position (colonne, hauteur)
-    
-    public void Game() {
-        
+public class Game {
+
+    private byte[][] matrix; // matrice des pions de 1-7 (largeur) 1-6, 1-7 (hauteur)
+    private int winner; // 0: null, 1 or 2 the winner
+    private int lastPlayer;
+    private byte[] lastCell; // stock la derniere position (colonne, hauteur)
+
+    public Game() {
+        this.winner = 0;
+        this.lastPlayer = 0;
+        this.matrix = new byte[7][6];
+        this.lastCell = new byte[2];
     }
 
+    public boolean action(int numJoueur, int coup) {
+        return placePawn(numJoueur, (byte) coup);
+    }
+
+    private boolean placePawn(int numJoueur, byte col) {
+   		boolean done = false;
+   		for(byte i = 5; i > 0 ;i++){
+   	   	if ( matrix[i][col-1] == 0 ){
+   	      	this.lastCell[0] = i;
+   	      	this.lastCell[1] = (byte) (col-1);
+   	      	this.matrix[i][col-1] = (byte) numJoueur;
+   	      	done = true;
+   	     	 
+   	      	break;
+   	   	}
+   		}
+   		return done;
+   	 }
 
 
-@Override
-public void initialisation() {
-	this.winner = 0;
-	this.lastPlayer = 0;
-	this.matrix = new byte[7][6];
-	this.matrix[7][6] = {{0}}; 
-	this.lastCell = [0,0] ;
- 
+    public String status() {
+        //si game fini return zero
+        if (this.winner > 0) {
+            return String.valueOf(0);
+        } else {
+            return String.valueOf(this.lastPlayer);
+        }
+    }
+ public int vainqueur() {
+   		return this.winner;
+   	 }
+
+
+   	 public byte[] derniereAction() {
+   		return this.lastCell;
+   	 }
+
+   	 /**
+   	 *  Affiche la matrice
+   	 */
+   	 public void Affichage() {
+   		for (int i = 0; i < this.matrix.length; i++) {
+   		   	 for (int j = 0; j < this.matrix[i].length; j++) {
+   		   		 System.out.print(this.matrix[i][j] + " ");
+   		   	 }
+   		   	 System.out.println();
+   		}
+   	 }
+   	 
+   	 public static void main(String[] args) {
+   	 	Game p4 = new Game();
+   		 p4.Affichage();
+   		 System.out.println(p4.placePawn(1, (byte) 3));
+   	 }
 }
-
-@Override
-public boolean action(int numJoueur, int coup) {
-   return placePawn(numJoueur, (byte)coup);
-}
-
-private boolean placePawn(int numJoueur, byte col) {
-   bool done = false;
-   for(int i = 0; i < 6 ;i++){
-      if ( matrix[col-1][i] == 0 ){
-         lastCell = [col-1][i];
-         matrix[col-1][i] = (byte)numJoueur;
-         done = true;
-         
-         break;
-      }
-   }
-   return done;
-}
-
-
-@Override
-public String status() {
-   //si game fini return zero
-	if (this.winner>0){
-return String.valueOf(0)
-}else{
-		return String.valueOf(this.lastPlayer());
-}
-	
-
-}
-
-@Override
-public int vainqueur() {
-   return this.winner;
-}
-
-@Override
-public int derniereAction() {
-   return this.lastCell;
-}
-
-@Override
-/**
-*  Affiche la matrice
-*/
-public String Affichage() {
-   for (int i = 0; i < this.matrix.length; i++) { 
-for (int j = 0; j < this.matrix[i].length; j++) { 
-System.out.print(this.matrix[i][j] + " "); 
-} 
-System.out.println();
-
-}
-   
-}
-
-
