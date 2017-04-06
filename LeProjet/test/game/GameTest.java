4192c5e2dfd83368dfbd6106e22cb29b5c62b1b7
AugustinAmbiehl <ambiehlaugustin@gmail.com>
2017-04-06 11:03:50 +0100

    Correcting test BOOL to String

--- LeProjet/test/game/GameTest.java
+++ LeProjet/test/game/GameTest.java
@@ -75,7 +75,7 @@ public class GameTest {
         System.out.println("status");
         Game instance = new Game();
         boolean expResult = false;
-        boolean result = instance.status();
+        String result = instance.status();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
