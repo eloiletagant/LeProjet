fc74ba28e6780977c757ebb5d5e34ad6513b69e2
Alexis A <alexis.amiand@gmail.com>
2017-04-06 17:18:52 +0200

    checkwin

--- LeProjet/src/java/game/Game.java
+++ LeProjet/src/java/game/Game.java
@@ -28,7 +28,11 @@ public class Game {
         }
 
 	public boolean action(int numJoueur, int coup) {
-		return placePawn(numJoueur, (byte) coup);
+		boolean fait =  placePawn(numJoueur, (byte) coup);
+                if (fait){
+                    checkWin();  
+                }
+                return fait;
 	}
 
 	private boolean placePawn(int numJoueur, byte col) {
@@ -118,6 +122,145 @@ public class Game {
 	    sb.append("</table>");
 	    return sb.toString();
 	}
+        
+        public void checkWin(){
+
+            for(int i = 0;i<6 ;i++){
+                for (int j = 0; j<7 ;j++){
+                    System.out.print(matrix[j][5-i]+" | ");
+                }
+                System.out.println("");
+            }
+
+            int posx = lastCell[0];
+            int posy = lastCell[1];
+
+            System.out.println("lastCell : ["+posx+","+posy+"] = "+matrix[posx][posy]);
+
+            //vertical (|)
+            if(posy >= 3){
+                System.out.println("Check vertical");
+                for(int i = 0; i < 4;i++){
+                    System.out.println("["+posx+","+(posy-i)+"] = "+matrix[posx][posy-i]);
+                    if(matrix[posx][posy] == matrix[posx][posy-i] ){
+                        if(i == 3)
+                            this.winner = matrix[posx][posy];
+                    }else {
+                        System.out.println("Pas bon => STOP");
+                        break;
+                    }
+                }
+            }
+
+            //Horizontonal (-)
+            System.out.println("");
+            System.out.println("Horizontal");
+            int cursor = posx-3<0?0:posx-3;
+            int end = posx+3>6?6:posx+3;
+            int compteur = 0;
+            int i = 0;
+            while (true){
+                System.out.println("["+(cursor+i)+","+posy+"] = "+ matrix[cursor+i][posy]);
+                if(matrix[posx][posy] == matrix[cursor+i][posy] ){
+                    compteur++;
+                }else{
+                    compteur = 0;
+                }
+                if(compteur >=4){
+                    this.winner = matrix[posx][posy];
+                }
+                if(cursor+i >=6){
+                    break;
+                }
+                i++;
+            }
+
+            System.out.println("");
+            System.out.println("Slash (/)");
+            //slash (/)
+            int cursorx = posx;
+            int cursory = posy;
+            while (true){
+                if( cursorx <=0 || cursory <=0){
+                    break;
+                }
+                cursorx --;
+                cursory --;
+            }
+
+            int endx = posx;
+            int endy = posy;
+            while (true){
+                if( endx >=6 || endy >=5){
+                    break;
+                }
+                endx ++;
+                endy ++;
+            }
+
+
+            compteur = 0;
+            i=0;
+            while (true){
+                System.out.println("["+(cursorx+i)+","+(cursory+i)+"] = "+ matrix[cursorx+i][cursory+i]);
+                if(matrix[posx][posy] == matrix[cursorx+i][cursory+i] ){
+                    compteur++;
+                }else{
+                    compteur = 0;
+                }
+                if(compteur >=4){
+                    this.winner = matrix[posx][posy];
+                    break;
+                }
+                if(cursorx+i >= 6 || cursory+i >= 5){
+                    break;
+                }
+                i++;
+            }
+
+            //anti-slash (\)
+            System.out.println("");
+            System.out.println("Anti-slash (\\)");
+
+            cursorx = posx;
+            cursory = posy;
+            while (true){
+                if( cursorx <=0 || cursory >=5){
+                    break;
+                }
+                cursorx --;
+                cursory ++;
+            }
+
+            endx = posx;
+            endy = posy;
+            while (true){
+                if( endx >=6 || endy <=0){
+                    break;
+                }
+                endx ++;
+                endy --;
+            }
+
+            compteur = 0;
+            i = 0;
+            while (true){
+                System.out.print("["+(cursorx+i)+","+(cursory-i)+"] = ");
+                System.out.println(matrix[cursorx+i][cursory-i]);
+                if(matrix[posx][posy] == matrix[cursorx+i][cursory-i] ){
+                    compteur++;
+                }else{
+                    compteur = 0;
+                }
+                if(compteur >=4){
+                    this.winner = matrix[posx][posy];
+                }
+                if(cursorx+i >= 6|| cursory-i <= 0){
+                    break;
+                }
+                i++;
+            }
+        }
 
 	public static void main(String[] args) {
 		Game p4 = new Game();
